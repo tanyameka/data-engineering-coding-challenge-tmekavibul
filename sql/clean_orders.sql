bbe330c1c38e3e213dae389ceba8c1be6a858201
tanyameka <tanya.mekavibul@gmail.com>
2025-11-14 07:30:46 -0500

    Create clean_orders.sql

---
+++ sql/clean_orders.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE TABLE clean_order AS
+SELECT DISTINCT
+    c1 AS order_id,
+    c2 AS customer_id,
+    COALESCE(
+        TRY_TO_DATE(c3),
+        TRY_TO_DATE(c3, 'Mon DD YYYY'),
+        TRY_TO_DATE(c3, 'YYYY/MM/DD'),
+        TRY_TO_DATE(c3, 'MM-DD-YYYY')
+    ) AS LAST_UPDATED, 
+    COALESCE(NULLIF(c4, 'NULL'),0)::float AS order_total,
+    c5 AS status,
+    c6 AS payment_method
+FROM SOURCES.TEST_DATA.ORDERS
+WHERE c1 <> 'order_id';
+
+
+-- select * from clean_order
+
+-- SELECT order_id, COUNT(*) FROM 
+-- clean_order
+-- GROUP BY 
+-- order_id
+-- HAVING COUNT(*) > 1
